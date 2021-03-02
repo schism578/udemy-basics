aa08150e41a8ece6c0325b917c20e6b8935e0c7e
Adrian Marquis <adrian.marquis78@gmail.com>
2021-03-01 20:38:53 -0500

    added functions

---
+++ functions/functions.js
@@ -0,0 +1,21 @@
+//Create a function called 'sum'
+/*function sum() {
+    let num1 = 2;
+    let num2 = 3;
+    let sum = num1 + num2;
+
+    console.log('The total of ' + num1 + ' and ' + num2 + ' is ' + sum)
+}
+
+sum();*/
+
+//Can also be written as a 'function expression':
+let sum = function() {
+    let num1 = 2;
+    let num2 = 3;
+    let sum = num1 + num2;
+
+    console.log('The total of ' + num1 + ' and ' + num2 + ' is ' + sum)
+}
+
+sum();
