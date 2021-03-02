1763b3d3212c9a84ca652fbaff66ed31b50ae265
Adrian Marquis <adrian.marquis78@gmail.com>
2021-03-01 20:26:33 -0500

    added loop definitions in comments

--- while-loops.js
+++ while-loops.js
@@ -5,6 +5,18 @@ let test = false;
 let count = 0;
 
 //Create the loop; 'while(test)' implies that 'test == true' changing its original value
+//performs test on condition being true
+//versus 'for' loop which performs test on condition 'x' number of times
+while(test) {
+    if(count == 3) {
+        test = false;
+        break;
+    }
+    console.log('The count is: ' + count);
+    count++
+} 
+
+//do-while loop; test for true or false like 'while loop'
 do {
     if(count == 3) {
         test = false;
