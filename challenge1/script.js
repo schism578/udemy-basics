3bf53d45455a66c2926436630a4357360dc02b76
Adrian Marquis <adrian.marquis78@gmail.com>
2021-04-12 15:57:14 -0400

    querySelector module completed

--- challenge1/script.js
+++ challenge1/script.js
@@ -1,7 +1,23 @@
-let selectScript = document.querySelectorAll("p");
+/*let selectScript = document.querySelectorAll("p");
 
 selectScript.forEach(para => {
     if(para.textContent.includes("second")) {
         para.style.color = "blue";
     }
-});
+});  //returns the second paragraph in blue along with all of the others
+*/
+
+/*let mySelection = document.querySelector("#para2");
+
+mySelection.textContent = "This was changed by JavaScript";
+//returns: a new paragraph element with the above text in place of the original text
+*/
+
+//OR
+let mySelection = document.querySelectorAll(".myClass");
+
+mySelection.forEach(para => {
+    para.textContent = "JavaScript changed this";
+})  //returns: above text in place of the paragraph elements that contain the myClass class
+
+
