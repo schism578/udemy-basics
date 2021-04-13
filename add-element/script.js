b0d54db00610678134860790d4fc63691d98c4ad
Adrian Marquis <adrian.marquis78@gmail.com>
2021-04-13 09:55:54 -0400

    Adding HTML Elements module created

---
+++ add-element/script.js
@@ -0,0 +1,7 @@
+let p1 = document.createElement("p");
+p1.textContent = "This was created with JavaScript";
+document.querySelector("body").appendChild(p1);
+
+let p2 = document.createElement("p");
+p2.textContent = "This is the second paragraph";
+document.querySelector("body").appendChild(p2);
